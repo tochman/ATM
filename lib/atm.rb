1232d30cc0bdcc6aa51ef664e74300eb3be9554a
tochman <thomas@nocebo.se>
2016-02-17 14:35:51 +0100

    re-factored the atm class with new methods

--- lib/atm.rb
+++ lib/atm.rb
@@ -6,10 +6,27 @@ class ATM
   end
 
   def withdraw(amount)
-    if @balance > amount
-      { status: true, message: 'success', date: Date.today.strftime("%Y-%m-%d")}
+    case
+    when not_divisible_by_five?(amount)
+      message(false, 'Amount not divisible by 5')
+    when insufficient_funds_in_atm?(amount)
+      message(false, 'Insufficient funds')
     else
-      { status: false, message:'Insufficient funds', date: Date.today.strftime("%Y-%m-%d")}
+      message(true, 'Success')
     end
+  end
+
+  private
+
+  def insufficient_funds_in_atm?(amount)
+    @balance < amount
+  end
+
+  def not_divisible_by_five?(amount)
+    amount % 5 != 0
+  end
+
+  def message(status, message)
+    { status: status, message: message, date: Date.today.strftime('%Y-%m-%d') }
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -6,13 +6,18 @@ describe ATM do
   end
 
   it 'returns a sucsessful message' do
-    message = { status: true, message: 'success', date: Date.today.strftime("%Y-%m-%d")}
+    message = { status: true, message: 'Success', date: Date.today.strftime('%Y-%m-%d') }
     expect(subject.withdraw(100)).to eq message
   end
 
   it 'rejects a withdrawal if balance = 0' do
     subject.balance = 0
-    message = { status: false, message:'Insufficient funds', date: Date.today.strftime("%Y-%m-%d")}
+    message = { status: false, message: 'Insufficient funds', date: Date.today.strftime('%Y-%m-%d') }
     expect(subject.withdraw(100)).to eq message
+  end
+
+  it 'rejects a withdrawal if amount is not divisible by 5' do
+    message = { status: false, message: 'Amount not divisible by 5', date: Date.today.strftime('%Y-%m-%d') }
+    expect(subject.withdraw(9)).to eq message
   end
 end
