dae86684c3357f905bad02828a2770c15081de59
tarekalhamad <tarek.alhamad@yahoo.com>
2016-02-17 13:54:33 +0100

    WIP ATM added method to ATM ping over to Thomas

---
+++ lib/atm.rb
@@ -0,0 +1,5 @@
+class ATM
+ def withdraw(amount)
+  { status: true, message: 'success' }
+ end
+end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,9 @@
 require './lib/atm.rb'
 
 describe ATM do
+  subject do
+    described_class.new(balance:1000)
+  end
   it 'returns a sucsessful message' do
     message = { status: true, message: 'success' }
     expect(subject.withdraw(100)).to eq message
