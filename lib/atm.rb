cea16231142e69d875782933f6127160458fb13a
tochman <thomas@nocebo.se>
2016-02-17 14:02:19 +0100

    WIP ATM - added test for rejection of withdrawal. ping over to Tarek

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,5 +1,15 @@
 class ATM
- def withdraw(amount)
-  { status: true, message: 'success' }
- end
+  attr_accessor :balance
+
+  def initialize(options = {})
+    @balance = options[:balance]
+  end
+
+  def withdraw(amount)
+    if @balance > amount
+      { status: true, message: 'success' }
+    else
+      { status: false }
+    end
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,10 +2,17 @@ require './lib/atm.rb'
 
 describe ATM do
   subject do
-    described_class.new(balance:1000)
+    described_class.new(balance: 1000)
   end
+
   it 'returns a sucsessful message' do
     message = { status: true, message: 'success' }
+    expect(subject.withdraw(100)).to eq message
+  end
+
+  it 'rejects a withdrawal if balance = 0' do
+    subject.balance = 0
+    message = { status: false }
     expect(subject.withdraw(100)).to eq message
   end
 end
