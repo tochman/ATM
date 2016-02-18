e386bbbc1821181e59d87945436b532dd4c19099
tochman <thomas@nocebo.se>
2016-02-18 11:30:54 +0100

    WIP ATM class #withdraw method

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,14 +5,20 @@ class ATM
     @balance = options[:balance]
   end
 
-  def withdraw(amount)
+  def withdraw(account_object, entered_pin_number, amount)
     case
-    when not_divisible_by_five?(amount)
-      message(status: false, message: 'Amount not divisible by 5')
-    when insufficient_funds_in_atm?(amount)
-      message(status: false, message: 'Insufficient funds')
-    else
-      message(status: true, message: 'Success', amount: amount)
+      when not_divisible_by_five?(amount)
+        message(status: false, message: 'Amount not divisible by 5')
+      when insufficient_funds_in_atm?(amount)
+        message(status: false, message: 'Insufficient funds')
+      when is_entred_pin_number_invalid?(account_object, entered_pin_number)
+        message()
+      when is_card_expired?(account_object)
+        message()
+      when is_cad_inactive?(account_object)
+        message()
+      else
+        message(status: true, message: 'Success', amount: amount)
     end
   end
 
@@ -27,32 +33,37 @@ class ATM
   end
 
   def message(options = {})
-    message = { status: options[:status], message: options[:message], date: Date.today.strftime('%Y-%m-%d') }
+    message = {status: options[:status], message: options[:message], date: Date.today.strftime('%Y-%m-%d')}
     message.merge!({amount: options[:amount]}) if options[:amount]
     message
+  end
+
+  def is_entred_pin_number_invalid?(account, pin)
+    account.pin_number != pin
   end
 end
 
 
 class Person
   attr_accessor :name, :address, :pin_number, :expir_date, :card_status, :ammount_cash
+
   def initialize(options = {})
-      @name = options[:name]
-      @address = options[:address]
-      @pin_number = options[:pin_number]
-      @expir_date = options[:expir_date]
-      @card_status = options[:card_status]
-      @ammount_cash = options[:ammount_cash]
+    @name = options[:name]
+    @address = options[:address]
+    @pin_number = options[:pin_number]
+    @expir_date = options[:expir_date]
+    @card_status = options[:card_status]
+    @ammount_cash = options[:ammount_cash]
   end
 
   def checkstatus(options = {})
-      if options[:name]
-        message = { status: false, message: 'No name for pereson', date: Date.today.strftime('%Y-%m-%d')}
-      elsif options[:address]
-        message = { status: false, message: 'No adderss for pereson', date: Date.today.strftime('%Y-%m-%d')}
-      elsif options[:ammount_cash]
-        message = { status: false, message: 'Negative amount for cash', date: Date.today.strftime('%Y-%m-%d')}
-      end
+    if options[:name]
+      message = {status: false, message: 'No name for pereson', date: Date.today.strftime('%Y-%m-%d')}
+    elsif options[:address]
+      message = {status: false, message: 'No adderss for pereson', date: Date.today.strftime('%Y-%m-%d')}
+    elsif options[:ammount_cash]
+      message = {status: false, message: 'Negative amount for cash', date: Date.today.strftime('%Y-%m-%d')}
+    end
   end
 
 end
